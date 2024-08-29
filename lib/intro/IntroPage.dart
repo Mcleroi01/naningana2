73c1665be0da2c0415e341643b0b39c36f662d47
mcleroi01 <musongelacrlo54@gmail.com>
2024-08-29 16:03:43 +0100

    mise place du welecome page

--- lib/intro/IntroPage.dart
+++ lib/intro/IntroPage.dart
@@ -48,30 +48,174 @@ class _IntroPageState extends State<IntroPage> {
 
     // Définir les motifs de couleur fixes
     final List<List<Color>> colorPatterns = [
-      [Colors.red, Colors.blue, Colors.yellow, Colors.red, Colors.blue, Colors.yellow],
-      [Colors.blue, Colors.yellow, Colors.red, Colors.blue, Colors.yellow, Colors.red],
-      [Colors.yellow, Colors.red, Colors.blue, Colors.yellow, Colors.red, Colors.blue],
-
+      [
+        Colors.red,
+        Colors.blue,
+        Colors.yellow,
+        Colors.red,
+        Colors.blue,
+        Colors.yellow
+      ],
+      [
+        Colors.blue,
+        Colors.yellow,
+        Colors.red,
+        Colors.blue,
+        Colors.yellow,
+        Colors.red
+      ],
+      [
+        Colors.yellow,
+        Colors.red,
+        Colors.blue,
+        Colors.yellow,
+        Colors.red,
+        Colors.blue
+      ],
     ];
 
     // Définir les instructions pour chaque bloc
     final List<Map<String, dynamic>> instructions = [
-      {"bloc": 13, "color": Colors.red, "instruction": "PA: Je pose mon pied gauche sur le rouge en dansant", "highlight": [13]},
-      {"bloc": 18, "color": Colors.yellow, "instruction": "AD: Je pose mon pied droit sur le jaune en dansant  ", "highlight": [18]},
-      {"bloc": 14, "color": Colors.red, "instruction": "PA: Je pose mon 2e pied sur la couleur à  côté et je danse en balançant les bras.", "highlight": [14]},
-      {"bloc": 17, "color": Colors.yellow, "instruction": "AD: Je pose mon 2e pied sur la couleur à  côté et je danse en balançant les bras", "highlight": [17]},
-      {"bloc": 8, "color": Colors.red, "instruction": "PA: J'avance sur la couleur de mon 2e pied et je tapotte mes cuisses en dansant.   ", "highlight": [8]},
-      {"bloc": 12, "color": Colors.yellow, "instruction": "AD: J'avance sur la couleur de mon 2e pied et je tapotte mes cuisses en dansant.   ", "highlight": [12]},
-      {"bloc": 7, "color": Colors.red, "instruction": "PA: J'avance sur la couleur qui était à côté de ma jambe droite et je tourne sur moi en balançant mes bras. ", "highlight": [7]},
-      {"bloc": 11, "color": Colors.yellow, "instruction": "AD: J'avance sur la couleur qui était à côté de ma jambe droite et je tourne sur moi en balançant mes bras. ", "highlight": [11]},
-      {"bloc": 9, "color": Colors.red, "instruction": "PA: Je pose mon pied gauche sur le rouge en dansant", "highlight": [9]},
-      {"bloc": 10, "color": Colors.yellow, "instruction": "AD: Je pose mon pied droit sur le jaune en dansant  ", "highlight": [10]},
-      {"bloc": 11, "color": Colors.red, "instruction": "PA: Je pose mon 2e pied sur la couleur à  côté et je danse en balançant les bras.", "highlight": [11]},
-      {"bloc": 12, "color": Colors.yellow, "instruction": "AD: Je pose mon 2e pied sur la couleur à  côté et je danse en balançant les bras", "highlight": [12]},
-      {"bloc": 13, "color": Colors.red, "instruction": "PA: J'avance sur la couleur de mon 2e pied et je tapotte mes cuisses en dansant.   ", "highlight": [13]},
-      {"bloc": 14, "color": Colors.yellow, "instruction": "AD: J'avance sur la couleur de mon 2e pied et je tapotte mes cuisses en dansant.   ", "highlight": [14]},
-      {"bloc": 15, "color": Colors.red, "instruction": "PA: J'avance sur la couleur qui était à côté de ma jambe droite et je tourne sur moi en balançant mes bras. ", "highlight": [15]},
-      {"bloc": 16, "color": Colors.yellow, "instruction": "AD: J'avance sur la couleur qui était à côté de ma jambe droite et je tourne sur moi en balançant mes bras. ", "highlight": [16]}
+      {
+        "bloc": 13,
+        "color": Colors.red,
+        "instruction": "PA: Je pose mon pied gauche sur le rouge en dansant",
+        "highlight": [13],
+        "image":
+            "https://i.pinimg.com/originals/57/e2/09/57e209296e586933febadf06e271a3d3.gif"
+      },
+      {
+        "bloc": 18,
+        "color": Colors.yellow,
+        "instruction": "AD: Je pose mon pied droit sur le jaune en dansant  ",
+        "highlight": [18],
+        "image":
+            "https://i.gifer.com/3sjq.gif"
+      },
+      {
+        "bloc": 14,
+        "color": Colors.red,
+        "instruction":
+            "PA: Je pose mon 2e pied sur la couleur à  côté et je danse en balançant les bras.",
+        "highlight": [14],
+        "image":
+            "https://www.icegif.com/wp-content/uploads/2022/01/icegif-1456.gif"
+      },
+      {
+        "bloc": 17,
+        "color": Colors.yellow,
+        "instruction":
+            "AD: Je pose mon 2e pied sur la couleur à  côté et je danse en balançant les bras",
+        "highlight": [17],
+        "image":
+            "https://www.icegif.com/wp-content/uploads/2022/01/icegif-1456.gif"
+      },
+      {
+        "bloc": 8,
+        "color": Colors.red,
+        "instruction":
+            "PA: J'avance sur la couleur de mon 2e pied et je tapotte mes cuisses en dansant.   ",
+        "highlight": [8],
+        "image":
+            "https://i.pinimg.com/originals/57/e2/09/57e209296e586933febadf06e271a3d3.gif"
+      },
+      {
+        "bloc": 12,
+        "color": Colors.yellow,
+        "instruction":
+            "AD: J'avance sur la couleur de mon 2e pied et je  tapotte mes cuisses en dansant.   ",
+        "highlight": [12],
+        "image":
+            "https://media1.giphy.com/media/fXgNQdnq8Bo96tcZAT/giphy.gif?cid=6c09b952aftcmbssi9tnh2fpu9wjdg3hxyz0pc619h6f486s&amp;ep=v1_internal_gif_by_id&amp;rid=giphy.gif&amp;ct=s"
+      },
+      {
+        "bloc": 7,
+        "color": Colors.red,
+        "instruction":
+            "PA: J'avance sur la couleur qui était à côté de ma jambe droite et je tourne sur moi en balançant mes bras. ",
+        "highlight": [7],
+        "image":
+            "https://i.pinimg.com/originals/57/e2/09/57e209296e586933febadf06e271a3d3.gif"
+      },
+      {
+        "bloc": 11,
+        "color": Colors.yellow,
+        "instruction":
+            "AD: J'avance sur la couleur qui était à côté de ma jambe droite et je tourne sur moi en balançant mes bras. ",
+        "highlight": [11],
+        "image":
+            "https://i.pinimg.com/originals/57/e2/09/57e209296e586933febadf06e271a3d3.gif"
+      },
+      {
+        "bloc": 9,
+        "color": Colors.red,
+        "instruction": "PA: Je pose mon pied gauche sur le rouge en dansant",
+        "highlight": [9],
+        "image":
+            "https://i.pinimg.com/originals/57/e2/09/57e209296e586933febadf06e271a3d3.gif"
+      },
+      {
+        "bloc": 10,
+        "color": Colors.yellow,
+        "instruction": "AD: Je pose mon pied droit sur le jaune en dansant  ",
+        "highlight": [10],
+        "image":
+            "https://i.pinimg.com/originals/57/e2/09/57e209296e586933febadf06e271a3d3.gif"
+      },
+      {
+        "bloc": 11,
+        "color": Colors.red,
+        "instruction":
+            "PA: Je pose mon 2e pied sur la couleur à  côté et je danse en balançant les bras.",
+        "highlight": [11],
+        "image":
+            "https://www.icegif.com/wp-content/uploads/2022/01/icegif-1456.gif"
+      },
+      {
+        "bloc": 12,
+        "color": Colors.yellow,
+        "instruction":
+            "AD: Je pose mon 2e pied sur la couleur à  côté et je danse en balançant les bras",
+        "highlight": [12],
+        "image":
+            "https://i.pinimg.com/originals/57/e2/09/57e209296e586933febadf06e271a3d3.gif"
+      },
+      {
+        "bloc": 13,
+        "color": Colors.red,
+        "instruction":
+            "PA: J'avance sur la couleur de mon 2e pied et je tapotte mes cuisses en dansant.   ",
+        "highlight": [13],
+        "image":
+            "https://i.pinimg.com/originals/57/e2/09/57e209296e586933febadf06e271a3d3.gif"
+      },
+      {
+        "bloc": 14,
+        "color": Colors.yellow,
+        "instruction":
+            "AD: J'avance sur la couleur de mon 2e pied et je tapotte mes cuisses en dansant.   ",
+        "highlight": [14],
+        "image":
+            "https://i.pinimg.com/originals/57/e2/09/57e209296e586933febadf06e271a3d3.gif"
+      },
+      {
+        "bloc": 15,
+        "color": Colors.red,
+        "instruction":
+            "PA: J'avance sur la couleur qui était à côté de ma jambe droite et je tourne sur moi en balançant mes bras. ",
+        "highlight": [15],
+        "image":
+            "https://i.pinimg.com/originals/57/e2/09/57e209296e586933febadf06e271a3d3.gif"
+      },
+      {
+        "bloc": 16,
+        "color": Colors.yellow,
+        "instruction":
+            "AD: J'avance sur la couleur qui était à côté de ma jambe droite et je tourne sur moi en balançant mes bras. ",
+        "highlight": [16],
+        "image":
+            "https://i.pinimg.com/originals/57/e2/09/57e209296e586933febadf06e271a3d3.gif"
+      }
       // Ajoutez d'autres instructions ici
     ];
 
@@ -89,25 +233,25 @@ class _IntroPageState extends State<IntroPage> {
         List<int> highlight = instructionIndex < instructions.length
             ? instructions[instructionIndex]["highlight"]
             : [];
+        String image = instructionIndex < instructions.length
+            ? instructions[instructionIndex]["image"]
+            : "";
 
         _instructions.add({
           "bloc": (2 - i) * lineColors.length + j + 1,
           "color": lineColors[j],
           "instruction": instructionText,
-          "highlight": highlight
+          "highlight": highlight,
+          "image": image // Ajoutez l'URL de l'image ici
         });
 
         instructionIndex++;
       }
     }
 
-
-    print('Instructions générées: $_instructions'); // Débogage : Vérifiez si les instructions sont générées
+    print(
+        'Instructions générées: $_instructions'); // Débogage : Vérifiez si les instructions sont générées
   }
-
-
-
-
 
   void _startGame() {
     _startGlobalTimer();
@@ -145,14 +289,15 @@ class _IntroPageState extends State<IntroPage> {
         _currentInstructionIndex++;
         final currentInstruction = _instructions[_currentInstructionIndex];
         _highlightedBlocks = List<int>.from(currentInstruction["highlight"]);
-        _instructionTimeLeft = (_globalTimeLeft / (_instructions.length - _currentInstructionIndex)).floor();
+        _instructionTimeLeft = (_globalTimeLeft /
+                (_instructions.length - _currentInstructionIndex))
+            .floor();
       });
       _startInstructionTimer();
     } else {
       _endGame();
     }
   }
-
 
   void _endGame() {
     _globalTimer?.cancel();
@@ -161,7 +306,11 @@ class _IntroPageState extends State<IntroPage> {
       context: context,
       builder: (context) => AlertDialog(
         title: Text("Fin du jeu"),
-        content: Text("Le jeu est terminé !"),
+        content: Column(
+          children: [
+            Text("Le jeu est terminé !"),
+          ],
+        ),
         actions: [
           TextButton(
             onPressed: () {
@@ -226,12 +375,12 @@ class _IntroPageState extends State<IntroPage> {
                   _header(),
                   SizedBox(height: 30.0),
                   _quiz(instruction),
-                  SizedBox(height: 50.0),
+                  SizedBox(
+                    height: 50,
+                  ),
                   Expanded(
                     child: Container(
-                        decoration: BoxDecoration(
-                          color: Colors.black26
-                        ),
+                        decoration: BoxDecoration(color: Colors.black26),
                         child: _palleteGame()),
                   ),
                   _bottomButtons(),
@@ -268,7 +417,10 @@ class _IntroPageState extends State<IntroPage> {
                 ),
                 child: Padding(
                   padding: const EdgeInsets.all(8.0),
-                  child: Icon(Icons.volume_down,color: Colors.lightBlue,),
+                  child: Icon(
+                    Icons.volume_down,
+                    color: Colors.lightBlue,
+                  ),
                 ),
               ),
               Container(
@@ -301,7 +453,10 @@ class _IntroPageState extends State<IntroPage> {
                 ),
                 child: Padding(
                   padding: const EdgeInsets.all(8.0),
-                  child: Icon(Icons.info),
+                  child: Icon(
+                    Icons.info,
+                    color: Colors.lightBlue,
+                  ),
                 ),
               ),
             ],
@@ -315,28 +470,62 @@ class _IntroPageState extends State<IntroPage> {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 25.0),
       child: Container(
-        height: 150.0,
+        padding: const EdgeInsets.all(10.0),
         decoration: BoxDecoration(
           color: Colors.black38,
           borderRadius: BorderRadius.circular(30.0),
         ),
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Text(
-              instruction["instruction"],
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 22,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Afficher l'image associée à l'instruction
+            if (instruction["image"] != null && instruction["image"].isNotEmpty)
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10.0),
+                child: Image.network(
+                  instruction["image"],
+                  height: 50.0, // Ajustez la hauteur de l'image
+                  width: 50.0, // Ajustez la largeur de l'image
+                  fit: BoxFit.cover, // Ajuste l'image pour couvrir le conteneur
+                  loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent? loadingProgress) {
+                    if (loadingProgress == null) {
+                      return child;
+                    } else {
+                      return Center(
+                        child: CircularProgressIndicator(
+                          value: loadingProgress.expectedTotalBytes != null
+                              ? loadingProgress.cumulativeBytesLoaded / (loadingProgress.expectedTotalBytes ?? 1)
+                              : null,
+                        ),
+                      );
+                    }
+                  },
+                  errorBuilder: (BuildContext context, Object error, StackTrace? stackTrace) {
+                    return Center(
+                      child: Icon(Icons.error, color: Colors.red, size: 40.0),
+                    );
+                  },
+                ),
+              ),
+            // Afficher le texte de l'instruction
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text(
+                instruction["instruction"],
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
               ),
             ),
-          ),
+          ],
         ),
       ),
     );
   }
+
 
   Widget _palleteGame() {
     return Padding(
@@ -348,7 +537,8 @@ class _IntroPageState extends State<IntroPage> {
         itemCount: _instructions.length,
         itemBuilder: (context, index) {
           final instruction = _instructions[index];
-          final isHighlighted = _highlightedBlocks.contains(instruction["bloc"]);
+          final isHighlighted =
+              _highlightedBlocks.contains(instruction["bloc"]);
 
           return Container(
             margin: EdgeInsets.all(2.0),
@@ -356,13 +546,16 @@ class _IntroPageState extends State<IntroPage> {
               color: instruction["color"],
               borderRadius: BorderRadius.circular(4.0),
               border: isHighlighted
-                  ? Border.all(color: Colors.green, width: 4.0) // Bordure rouge pour mise en évidence
+                  ? Border.all(
+                      color: Colors.green,
+                      width: 4.0) // Bordure rouge pour mise en évidence
                   : null,
             ),
             child: Center(
               child: Text(
                 instruction["bloc"].toString(),
-                style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
               ),
             ),
           );
@@ -371,7 +564,20 @@ class _IntroPageState extends State<IntroPage> {
     );
   }
 
+  Widget _imageGrid() {
+    return ListView.builder(
+      itemCount: _instructions.length,
+      itemBuilder: (context, index) {
+        final instruction = _instructions[index];
 
+        return ListTile(
+          leading: Image.network(instruction["image"], width: 50, height: 50),
+          // Affichage de l'image
+          title: Text(instruction["instruction"]),
+        );
+      },
+    );
+  }
 
   Widget _bottomButtons() {
     return Padding(
@@ -381,7 +587,8 @@ class _IntroPageState extends State<IntroPage> {
         children: [
           ElevatedButton(
             onPressed: () {
-              Navigator.pushNamed(context, '/h');
+              _endGame();
+              Navigator.pushNamed(context, '/home_page');
             },
             style: ElevatedButton.styleFrom(
               backgroundColor: Colors.red,
@@ -391,7 +598,10 @@ class _IntroPageState extends State<IntroPage> {
             ),
             child: Padding(
               padding: const EdgeInsets.all(15.0),
-              child: Icon(Icons.arrow_back,color: Colors.white,),
+              child: Icon(
+                Icons.output_sharp,
+                color: Colors.white,
+              ),
             ),
           ),
           ElevatedButton(
@@ -404,7 +614,10 @@ class _IntroPageState extends State<IntroPage> {
             ),
             child: Padding(
               padding: const EdgeInsets.all(15.0),
-              child: Icon(Icons.arrow_forward,color: Colors.white,),
+              child: Icon(
+                Icons.arrow_forward,
+                color: Colors.white,
+              ),
             ),
           ),
         ],

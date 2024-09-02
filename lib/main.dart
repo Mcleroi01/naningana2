10793dc52a714ab74448ba17512708c0b613a3fc
Pathel7 <mpoleshapaulin70@gmail.com>
2024-09-02 15:40:08 +0100

    Ajout du splashScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:naningana/intro/IntroPage.dart';
 import 'package:naningana/pages/FicheDeSuivi.dart';
@@ -20,7 +21,10 @@ void main() async{
     options: DefaultFirebaseOptions.currentPlatform,
   );
   await GetStorage.init();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(MyApp());
+  FlutterNativeSplash.remove();
 }
 
 class MyApp extends StatelessWidget {
